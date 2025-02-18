69d89a2d33f1926bf28b90c226e2d1158a228b31
Donut-os <119637477+Donut-os@users.noreply.github.com>
2025-02-19 08:58:55 +1100

    Update DB_Login.js

--- DB_Login.js
+++ DB_Login.js
@@ -1,8 +1,17 @@
-import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Replace the following with your app's Firebase project configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  //...
+  apiKey: "AIzaSyAaFOEJNoFeutS0w7h9_u0WHVwc52yATWU",
+  authDomain: "ct110-30ef0.firebaseapp.com",
+  projectId: "ct110-30ef0",
+  storageBucket: "ct110-30ef0.firebasestorage.app",
+  messagingSenderId: "485722976914",
+  appId: "1:485722976914:web:b6e4a872a1f3b4e12cac4b"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
